d866cc4d6ca3439b9ae90aa32ca0f2b6b18b0c10
jgage930 <jgage930@gmail.com>
2025-01-16 10:26:39 -0600

    feed mux

--- main.go
+++ main.go
@@ -1,20 +1,58 @@
 package main
 
 import (
-  "net/http"
-  "log"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
-  port := ":8080"
-  log.Printf("Starting Application.")
+	port := ":8080"
+	log.Printf("Starting Application.")
 
-  http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Podcast Server v0.0.1"))
-  })
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Podcast Server v0.0.1"))
+	})
 
-  err := http.ListenAndServe(port, nil)
-  if err != nil {
-    log.Fatal(err)
-  }
+	router := feedRouter(&FeedHandler{})
+
+	server := &http.Server{
+		Addr:    port,
+		Handler: router,
+	}
+
+	server.ListenAndServe()
+}
+
+func feedRouter(h *FeedHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /", h.CreateFeed)
+
+	return mux
+}
+
+type Feed struct {
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
+
+type FeedHandler struct{}
+
+func (h *FeedHandler) CreateFeed(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	var feed Feed
+	if err := json.Unmarshal(body, &feed); err != nil {
+		http.Error(w, "Failed to parse json Body", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	log.Printf("Recieved feed %s", feed.Name)
 }
